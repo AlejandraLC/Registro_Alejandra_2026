60a52e408312325e6db91c4c127447a81358f6f4
AlejandraLC <alex5abr82@gmail.com>
2026-02-18 20:28:44 -0600

    listo

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'registro-academico-v1';
+const ASSETS = [
+    './Index.html',
+    './styles.css',
+    './icon.svg',
+    './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => response || fetch(event.request))
+    );
+});
