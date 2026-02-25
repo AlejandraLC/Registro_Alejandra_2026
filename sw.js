d2f15c6d167173d7ac3ca8e9b7d9d0cf08c66211
AlejandraLC <alex5abr82@gmail.com>
2026-02-24 21:11:46 -0600

    Fix cache issues and update service worker to v3

--- sw.js
+++ sw.js
@@ -1,19 +1,47 @@
-const CACHE_NAME = 'registro-academico-v2';
+const CACHE_NAME = 'registro-academico-v3';
 const ASSETS = [
     './index.html',
     './styles.css',
     './icon.svg',
-    './manifest.json'
+    './manifest.json',
+    './asistencia.html',
+    './cotidiano.html',
+    './tareas.html',
+    './pruebas.html',
+    './analisis.html',
+    './estudiantes.html',
+    './diagnostico.html',
+    './comunicados.html',
+    './bitacora.html',
+    './calendario.html',
+    './config.html'
 ];
 
 self.addEventListener('install', (event) => {
+    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
     );
 });
 
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then((response) => response || fetch(event.request))
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
     );
 });
